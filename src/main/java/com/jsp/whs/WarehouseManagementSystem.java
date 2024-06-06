f281a0f6ff1eea9aa9e0e5845fbcb728ba3b54a7
SnehaManick <snehamanickam153@gmail.com>
2024-06-06 10:31:14 +0530

    Warehouse .gitignore Created

--- src/main/java/com/jsp/whs/WarehouseManagementSystem.java
+++ src/main/java/com/jsp/whs/WarehouseManagementSystem.java
@@ -1,3 +1,4 @@
+
 package com.jsp.whs;
 
 import org.springframework.boot.SpringApplication;
@@ -6,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class  WarehouseManagementSystem {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run( WarehouseManagementSystem.class, args);
 	}
